d60dc22a29c13a90c4abce33a38c07de5c01275d
Gabriel Szasz <gszasz@redhat.com>
2016-10-04 16:38:36 +0200

    License project under GNU GPL v3
    Although some parts of the code were licensed under GNU GPL v2 or v3,
    whole project was remaining unlicensed.  This is generally bad idea,
    especially when I want let other people freely modify and (re)distribute
    my code.  Licensing whole project under GNU GPL v3 solves this issue.

--- lisp/sublime-text-2.el
+++ lisp/sublime-text-2.el
@@ -1,3 +1,39 @@
+;;; sublime-text-2.el --- Sublime Text 2 theme for Emacs
+
+;; Copyright (C) 2016 Gabriel Szasz
+
+;; Author: Gabriel Szasz <gabriel.szasz1@gmail.com>
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or
+;; Modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; To use the theme, add following lines to .emacs or init.el file:
+;;
+;;   (require 'sublime-text-2)
+;;   (sublime-text-2)
+;;
+;; It's more convenient to invoke the theme only in graphic window:
+;;
+;;   (when (or (daemonp) (display-graphic-p))
+;;     (require 'sublime-text-2)
+;;     (sublime-text-2))
+
 (defun sublime-text-2 ()
   (interactive)
   (color-theme-install
