84da07984d663437931670f7a752b99b451cb1c1
Gabriel Szász <gabriel.szasz1@gmail.com>
2022-06-27 08:59:00 +0200

    Set AUCTeX to autosave buffer
    I have discovered that AUCTeX can autosave document when running
    TeX-command-run-all when TeX-save-query is set to nil.  This provides
    Overleaf-like experience that's probably even more convenient better
    than running 'latexmk -pvc' on background.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-save-query nil)
  '(before-save-hook '(copyright-update))
  '(blink-cursor-mode t)
  '(column-number-mode t)
