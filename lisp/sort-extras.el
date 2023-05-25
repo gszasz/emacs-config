66c6986ac891fb1ed8a9a024759f7b0ea98c5e2a
Gabriel Szász <gabriel.szasz1@gmail.com>
2023-05-25 17:37:00 -0400

    Add extra sort functions
    I am using sorting of words in region too often to do it in a multi-step
    process.  Noam Postavsky published in the EmacsWiki very nice functions
    that can tackle this problem very nicely:
    
      <https://www.emacswiki.org/emacs/SortWords>

---
+++ lisp/sort-extras.el
@@ -0,0 +1,46 @@
+;;; sort-extras.el --- set of functions for sorting
+
+;; Copyright (C) 2023  Gabriel Szász
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; Based on code at Emacs Wiki:
+;; <https://www.emacswiki.org/emacs/SortWords>
+;;   Copyright (C) 2019  Noam Postavsky
+
+;; Author: Gabriel Szász
+;; Version: 1.0
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; <https://www.gnu.org/licenses/>.
+
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+   Prefixed with negative \\[universal-argument], sorts in
+   reverse.
+
+   The variable `sort-fold-case' determines whether alphabetic
+   case affects the sort order.
+
+   See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
+
+(defun sort-symbols (reverse beg end)
+  "Sort symbols in region alphabetically, in REVERSE if negative.
+   See `sort-words'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\(\\sw\\|\\s_\\)+" "\\&" beg end))
+
+(provide 'sort-extras)
