1452557c71ad3c39ba34d29c2fcebb8c88e5b30c
Gabriel Szász <gabriel.szasz1@gmail.com>
2023-06-15 18:44:12 -0400

    Uninstall EIN
    It seems that EIN does not support widgets and its implementation is
    quite cumbersome and buggy.  I'm using Jupyter notebooks only for
    learning, prototyping, and data-science-related tasks and for these
    purposes is JupyterLab Desktop far more convenient to use.

--- custom.el
+++ custom.el
@@ -19,7 +19,6 @@
    '("e3c41651565cb624f772d25fbf12752b31610800041968d96c9aef5a3e8ead8e" default))
  '(cython-default-compile-format "python setup.py build_ext --inplace")
  '(dired-mode-hook '(hl-line-mode))
- '(ein:output-area-inlined-images t)
  '(emacs-lisp-mode-hook
    '(eldoc-mode flyspell-prog-mode whitespace-cleanup-mode set-fill-column-lisp))
  '(fill-column 80)
@@ -40,7 +39,7 @@
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(ada-mode adaptive-wrap apache-mode counsel-projectile csv-mode ein elpy flyspell-correct-ivy gnuplot ivy-bibtex magit-section markdown-mode ob-async olivetti org-gtd org-remark php-mode pkg-info projectile pygn-mode stripes syslog-mode use-package visual-fill-column vterm whitespace-cleanup-mode yascroll))
+   '(ada-mode adaptive-wrap apache-mode counsel-projectile csv-mode elpy flyspell-correct-ivy gnuplot ivy-bibtex magit-section markdown-mode ob-async olivetti org-gtd org-remark php-mode pkg-info projectile pygn-mode stripes syslog-mode use-package visual-fill-column vterm whitespace-cleanup-mode yascroll))
  '(recentf-mode t)
  '(safe-local-variable-values '((bibtex-dialect . biblatex)))
  '(scroll-bar-mode nil)
